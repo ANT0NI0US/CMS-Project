b170266c27f6a057e3049f2a4244c6f4189c842b
ANT0NI0US <38351110+ANT0NI0US@users.noreply.github.com>
2019-03-31 20:16:18 +0200

    Update CMS-Script.sql

--- CMS-Script.sql
+++ CMS-Script.sql
@@ -4,6 +4,7 @@ DROP TABLE Donor cascade constraints;
 DROP TABLE Activity cascade constraints;
 DROP TABLE Donates cascade constraints;
 DROP TABLE Campaign cascade constraints;
+DROP TABLE EMPOLYEE cascade constraints;
 
 CREATE TABLE Branch
 (BID number(3) primary key,
@@ -45,3 +46,13 @@ CampDate date,
 BID number(3) references Branch(BID),
 ActivityName varchar2(50) references Activity(ActName),
 constraint pkCamp primary key (CampDate, BID, ActivityName));
+                               
+CREATE TABLE Volunteer 
+(SSN NUMBER(10,0) primary key, 
+FNAME VARCHAR2(10) NOT NULL, 
+LNAME VARCHAR2(10) NOT NULL, 
+SEX CHAR(1), 
+EMAIL VARCHAR2(20), 
+MOBILENUMBER NUMBER(12,0) NOT NULL, 
+ADDRESS VARCHAR2(50),
+BID NUMBER(3) references Branch(BID));
